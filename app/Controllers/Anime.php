db2764a33914aaf16408f067d2a2fcd3974029f9
JorgeAGB <jorgegbellos@gmail.com>
2023-06-14 19:27:29 -0600

    xd

---
+++ app/Controllers/Anime.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Anime extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    /*
+    Endpoints needed
+
+        - Obtener el todos los animes
+        - Obtener un anime por id
+        - Obtener animes por genero
+        - Obtener
+    */
+}
